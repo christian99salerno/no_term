f4a555f880288e9f3469b785207a0a95a2fb9dc2
christian99salerno <christian.salerno05@gmail.com>
2017-05-04 18:52:42 +0200

    file testo, difficoltà

---
+++ thesum.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main ()
+{
+
+int num = 0;
+int num_input = 0;
+
+int somma = 0;
+
+ FILE *f = fopen("sum.dat", "r");
+ 
+   printf("Lettura in sum.dat in corso ...\n");
+   
+   fscanf(f, "%d", &num);
+   printf("Valore attuale in sum.dat: %d\n", num);
+ 
+   printf("Digita un valore da sommare: ");
+   scanf("%d", &num_input);  
+
+fclose(f);
+
+somma = num + num_input;
+
+printf("Nuovo valore: %d\n", somma);
+printf("Salvataggio in sum.dat in corso ...\n");
+
+FILE *f = fopen("sum.dat", "w");
+
+fprintf(f, somma);
+
+fclose(f);
+
+  return 0;
+}
+
