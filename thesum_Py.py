8326ccbffb07439ea3963927f53d32f4eb7e6b47
Billie99 <billiearmstrong1886@gmail.com>
2017-05-10 13:02:22 +0200

    Modifiche e capire ancora perche' non funziona in python

--- thesum_Py.py
+++ thesum_Py.py
@@ -1,39 +1,28 @@
 import sys
 
 f = open("sum.dat", "r")
-print f
-sys.exit(0)
+#print f
+#sys.exit(0)
 
 if not f:
-    num_input = input("Digita un valore da sommare: ")
-    
-    somma = num_input + 0
-
-    print "File inesistente, il valore e': ", somma
+    print "File inesistente, il valore di partenza e' 0"
 
 else:
     print "Lettura da sum.dat in corso ..."
-
     line = f.readline()
-
     num = int(line)
-
     print "Valore attuale in sum.dat: ", num
-
     f.close
 
-    num_input = input("Digita un valore da sommare: ")
+num_input = input("Digita un valore da sommare: ")
 
-    somma = num + num_input
+somma = num + num_input
 
-    print "Nuovo valore: ", somma
+print "Nuovo valore: ", somma
+print "Salvataggio in sum.dat in corso ..."
+f = open("sum.dat", "w")
+f.write(str(somma))
 
-    print "Salvataggio in sum.dat in corso ..."
-
-    f = open("sum.dat", "w")
-
-    f.write(str(somma))
-
-    f.close()
+f.close()
 
 
