1096fb1e05631902fc3e4e0bc2ac391351f7488c
Christian <christian.salerno05@gmail.com>
2018-10-22 10:48:14 +0200

    sum python

--- thesum_Py.py
+++ thesum_Py.py
@@ -2,28 +2,26 @@ import os
 
 num = 0
 
-file = os.path.isfile("sum.dat")
+fil = os.path.isfile("sum.dat")
 
-if not file:
-    print "File inesistente, il valore di partenza e' 0"
+if not fil:
+    print("File inesistente, il valore di partenza e' 0")
 
 else:
     f = open("sum.dat", "r")
-    print "Lettura da sum.dat in corso ..."
+    print("Lettura da sum.dat in corso ...")
     line = f.readline()
     num = int(line)
-    print "Valore attuale in sum.dat: ", num
+    print("Valore attuale in sum.dat: ", num)
     f.close
 
 num_input = input("Digita un valore da sommare: ")
 
 somma = num + num_input
 
-print "Nuovo valore: ", somma
-print "Salvataggio in sum.dat in corso ..."
+print("Nuovo valore: ", somma)
+print("Salvataggio in sum.dat in corso ...")
 f = open("sum.dat", "w")
 f.write(str(somma))
 
 f.close()
-
-
